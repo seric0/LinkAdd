99f7c18383353755b1a4a6b57497ccbfccd1c2d0
Seric0 <Pol1kz2020@gmail.com>
2022-05-13 17:52:24 +0600

    LinkAdd Programm Release v.1.0

---
+++ LinkAdd/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace LinkAdd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.reference = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.linkname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(253, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Получить название";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Название";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(504, 84);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(284, 46);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(202, 84);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(285, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.reference,
+            this.linkname});
+            this.dataGridView1.Location = new System.Drawing.Point(15, 189);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(800, 267);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "№ п\\п";
+            this.id.Name = "id";
+            this.id.Width = 50;
+            // 
+            // reference
+            // 
+            this.reference.HeaderText = "Ссылка";
+            this.reference.Name = "reference";
+            this.reference.Width = 300;
+            // 
+            // linkname
+            // 
+            this.linkname.HeaderText = "Название";
+            this.linkname.Name = "linkname";
+            this.linkname.Width = 400;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 468);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Ссылки";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn reference;
+        private System.Windows.Forms.DataGridViewTextBoxColumn linkname;
+    }
+}
+
---
+++ LinkAdd/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Data.SQLite;
+
+namespace LinkAdd
+{
+    public partial class Form1 : Form
+    {        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {                        
+            string pattern = @"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)";
+
+            if (textBox1.Text != "" && Regex.IsMatch(textBox1.Text, pattern, RegexOptions.IgnoreCase))
+            {
+                richTextBox1.Clear();
+                WebRequest myWebRequest = WebRequest.Create(textBox1.Text);
+                WebResponse myWebResponse = myWebRequest.GetResponse();
+                Stream ReceiveStream = myWebResponse.GetResponseStream();
+                Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+                StreamReader readStream = new StreamReader(ReceiveStream, encode);
+                Char[] read = new Char[256];
+                int count = readStream.Read(read, 0, 256);
+                progressBar1.Value = 0;
+                while (count > 0)
+                {
+                    if (progressBar1.Value != 100) { progressBar1.Value = progressBar1.Value + 1; }
+                    String str = new String(read, 0, count);
+                    count = readStream.Read(read, 0, 256);
+                    richTextBox1.AppendText(str);
+                }
+                string html_text = richTextBox1.Text;
+                int start = html_text.IndexOf("<title>");
+                int end = html_text.IndexOf("</title>", start);
+                string str1 = html_text.Substring(start, end - start + "</title>".Length);
+                string str2 = str1.Remove(0, 7);
+                int ind = str2.Length - 8;
+                string title = str2.Remove(ind);
+                label1.Text = title;
+                try
+                {
+                    string connectionString = @"Data Source=C:\Links\LinksDB.db";
+                    string sqlExpression = "INSERT INTO Link (reference, linkname) VALUES ('" + textBox1.Text + "', '" + label1.Text + "')";
+                    using (var connection = new SQLiteConnection(connectionString))
+                    {
+                        connection.Open();
+                        SQLiteCommand command = new SQLiteCommand();
+                        command.Connection = connection;
+                        command.CommandText = sqlExpression;
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (SQLiteException ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                Select_Value();
+                myWebResponse.Close();
+                readStream.Close();
+            }            
+            else
+            {
+                MessageBox.Show("Введите адрес сайта!");
+            }
+        }
+
+        public void Select_Value()
+        {
+            string sqlExpression = "SELECT * FROM Link";
+            using (var connection = new SQLiteConnection(@"Data Source=C:\Links\LinksDB.db"))
+            {
+                connection.Open();
+
+                SQLiteCommand command = new SQLiteCommand(sqlExpression, connection);
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows) // если есть данные
+                    {
+                        while (reader.Read())   // построчно считываем данные
+                        {
+                            var id = reader.GetValue(0);
+                            var reference = reader.GetValue(1);
+                            var linkname = reader.GetValue(2);
+                            dataGridView1.Rows.Add(id, reference, linkname);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(@"C:\Links\LinksDB.db"))
+            {
+                SQLiteConnection.CreateFile(@"C:\Links\LinksDB.db");
+            }
+            try
+            {
+                using (var connection = new SQLiteConnection(@"Data Source=C:\Links\LinksDB.db"))
+                {
+                    connection.Open();
+                    SQLiteCommand command = new SQLiteCommand();
+                    command.Connection = connection;
+                    command.CommandText = "CREATE TABLE IF NOT EXISTS Link (id INTEGER PRIMARY KEY AUTOINCREMENT, reference TEXT, linkname TEXT)";
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }                                   
+            Select_Value();
+        }
+    }
+}
